ba5bdee1bc90490fd35d633c05496ee43ed6a06b
tantalum4869 <tantalum590@gmail.com>
2019-10-06 17:58:14 +0700

    update

---
+++ footer.php
@@ -0,0 +1,133 @@
+<!-- Footer -->
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 class="font-weight-bold text-uppercase mb-4">M4U</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+          esse
+          quasi, veritatis totam voluptas nostrum.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Menu</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="#!">หน้าแรก</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">สินค้า</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">บริการ</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">เกี่ยวกับเรา</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="admin-login.php">ระบบ Admin</a>
+            </p>
+          </li>			
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 class="font-weight-bold text-uppercase mb-4">ติดต่อเรา</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fas fa-home mr-3"></i> เลขที่ 2316 ถนนประชาสงเคราะห์ แขวงดินแดง เขตดินแดง กทม. 10400</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-envelope mr-3"></i> xxxx@xxxxx.com</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-phone mr-3"></i> xxx xxx xxxx</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-print mr-3"></i> xx xxx xxx</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+        <!-- Facebook -->
+        <a type="button" class="btn-floating btn-fb">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <!-- Twitter -->
+        <a type="button" class="btn-floating btn-tw">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <!-- Google +-->
+        <a type="button" class="btn-floating btn-gplus">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+        <!-- Dribbble -->
+        <a type="button" class="btn-floating btn-dribbble">
+          <i class="fab fa-dribbble"></i>
+        </a>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href=""> M4U.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->	
